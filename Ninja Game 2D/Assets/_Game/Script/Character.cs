7cc51fa0e35eb2b6610e968c16bbd5551a2876a8
nongngocminh <nongngocminh.hanoi@gmail.com>
2025-06-12 12:41:50 +0700

    ~ Part 1 Anim Completed

--- Ninja Game 2D/Assets/_Game/Script/Character.cs
+++ Ninja Game 2D/Assets/_Game/Script/Character.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float speed = 250;
+    [SerializeField] private float jumpForce = 350;
 
-    private bool isGrounded;
-    private bool isJumping;
-    private bool isAttack;
+    private bool isGrounded = true;
+    private bool isJumping = false;
+    private bool isAttack = false;
 
+    private float horizontal;
+    private string currentAnimName;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +26,64 @@ public class Character : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        CheckGrounded();
+        isGrounded = CheckGrounded();
+
+        // -1 -> 0 -> 1
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (isAttack)
+        {
+            rb.velocity = Vector2.zero;
+            return;
+        }
+
+        if (isGrounded)
+        {
+            if (isJumping)
+            {
+                return;
+            }
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                Jump();
+            }
+
+            if (Mathf.Abs(horizontal) > 0.1f)
+            {
+                ChangeAnim("run");
+            }
+
+            if (Input.GetKeyDown(KeyCode.J) && isGrounded)
+            {
+                Attack();
+            }
+            
+            if (Input.GetKeyDown(KeyCode.K) && isGrounded)
+            {
+                Throw();
+            }
+        }
+
+        if (!isGrounded && rb.velocity.y < 0)
+        {
+            ChangeAnim("fall");
+            isJumping = false;
+        }
+
+        if (Mathf.Abs(horizontal) > 0.1f)
+        {
+            rb.velocity = new Vector2(horizontal * Time.fixedDeltaTime * speed, rb.velocity.y);
+
+            //transform.localScale = new Vector3(horizontal, 1, 1);
+            transform.rotation = Quaternion.Euler(new Vector3(0, horizontal > 0 ? 0 : 180, 0));
+        }
+        else if (isGrounded)
+        {
+            ChangeAnim("idle");
+            rb.velocity = Vector2.zero;
+        }
     }
 
     private bool CheckGrounded()
@@ -39,5 +101,42 @@ public class Character : MonoBehaviour
         //    return false; 
         //}
         return hit.collider != null;
+    }
+
+    private void Attack()
+    {
+        ChangeAnim("attack");
+        isAttack = true;
+        Invoke(nameof(ResetAttack), 0.5f);
+    }
+
+    private void Throw()
+    {
+        ChangeAnim("throw");
+        isAttack = true;
+        Invoke(nameof(ResetAttack), 0.5f);
+    }
+
+    private void Jump()
+    {
+        isJumping = true;
+        ChangeAnim("jump");
+        rb.AddForce(jumpForce * Vector2.up);
+    }
+
+    private void ResetAttack()
+    {
+        ChangeAnim("ilde");
+        isAttack = false;
+    }
+
+    private void ChangeAnim(string animName)
+    {
+        if (currentAnimName != animName) 
+        { 
+            animator.ResetTrigger(animName);
+            currentAnimName = animName;
+            animator.SetTrigger(currentAnimName);
+        }
     }
 }
